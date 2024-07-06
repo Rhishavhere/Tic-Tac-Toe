3448b7f85e6e390ce2315de072258c0b6dfbdf64
Rhishav <rhishh.me@gmail.com>
2024-07-06 08:59:18 +0530

    Player Section

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName]=useState(initialName)
+  const [isEditing, setIsEditing] = useState(false)
+
+  function handleEditClick() {
+    setIsEditing((editing)=>!editing) 
+  }
+
+  function handleChange(event){
+    setPlayerName(event.target.value)
+  }
+
+
+  let editableName = <span className="player-name">{playerName}</span>
+  let btnCaption = "Edit"
+
+  if (isEditing) {
+    editableName = <input type="text" required value={playerName}
+      onChange={handleChange} />
+    btnCaption = "Save"
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editableName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  )
+}
